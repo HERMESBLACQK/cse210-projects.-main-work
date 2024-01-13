b58bcaf788aaaf88fd1f2a27f77a541495701840
HermesBlacqk <omotosolilhenxy@gmail.com>
2024-01-13 16:00:01 +0100

    ....

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -32,14 +32,12 @@ class Program
         return Console.ReadLine();
     }
 
-    // Function to prompt the user for their favorite number and return it
     static int PromptUserNumber()
     {
         Console.Write("Please enter your favorite number: ");
         return int.Parse(Console.ReadLine());
     }
 
-    // Function to square a given number and return the result
     static int SquareNumber(int number)
     {
         return number * number;
