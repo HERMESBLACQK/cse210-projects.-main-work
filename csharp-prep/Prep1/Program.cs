24b1998cacf15ec0742c9227774d720469cdc0d5
HermesBlacqk <omotosolilhenxy@gmail.com>
2024-01-13 15:37:45 +0100

    .....

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,15 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        // This Prompt the user for their first name
         Console.Write("What is your first name? ");
         string firstName = Console.ReadLine();
 
-        // This Prompt the user for their last name
         Console.Write("What is your last name? ");
         string lastName = Console.ReadLine();
 
-        //This  Display the text back on one line
         Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
